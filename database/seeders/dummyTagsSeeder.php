a7791b5045adb96b32e61997ced3b72890fb0aec
y-web21 <y.web.2019@gmail.com>
2021-02-12 00:53:43 +0900

    データベース作成

---
+++ database/seeders/dummyTagsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class dummyTagsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $tags = ['戯言','無価値','風説の流布'];
+        foreach ($tags as $tag) {
+            DB::table('tags')->insert(['name' => $tag]);
+        }
+    }
+}
