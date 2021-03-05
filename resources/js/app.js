49ee9191e438052d8e6b46c21fdafac0779a864f
OdenMaster <y.web.2019@gmail.com>
2021-03-05 21:38:24 +0900

    誤記修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ function removeRequired() {
 
 const patch = '<input type="hidden" name="_method" value="patch">';
 const post = '<input type="hidden" name="_method" value="post">';
-const required_filed = ['new_title', 'new_content'];
+const required_field = ['new_title', 'new_content'];
 
 if (document.getElementById('btn_edit_confirm') != null) {
     document.getElementById('btn_edit_confirm').addEventListener('click', { handleEvent: insertFormMethod, method: patch });
@@ -27,8 +27,8 @@ if (document.getElementById('btn_edit_image_select') != null) {
     document.getElementById('btn_edit_image_select').addEventListener('click', { handleEvent: insertFormMethod, method: post });
 }
 if (document.getElementById('btn_submit_new_post') != null) {
-    document.getElementById('btn_submit_new_post').addEventListener('click', { handleEvent: addRequired, target: required_filed });
+    document.getElementById('btn_submit_new_post').addEventListener('click', { handleEvent: addRequired, target: required_field });
 }
 if (document.getElementById('btn_submit_select_image') != null) {
-    document.getElementById('btn_submit_select_image').addEventListener('click', { handleEvent: removeRequired, target: required_filed });
+    document.getElementById('btn_submit_select_image').addEventListener('click', { handleEvent: removeRequired, target: required_field });
 }
