70682ce0a62f61673780e36a7b158323738e10d3
OdenMaster <y.web.2019@gmail.com>
2021-03-06 04:55:44 +0900

    新規投稿と編集画面のblade共通化、画面移動ロジック共通化

---
+++ resources/views/poster/parts/layouts.blade.php
@@ -0,0 +1,4 @@
+<div class="w-full relative mt-4 shadow-2xl rounded my-24 overflow-hidden">
+    <div class="grid grid-cols-12 bg-white">
+    @include('poster.parts.side_nav_left')
+    <div class="col-span-12 md:border-solid md:border-l md:border-black md:border-opacity-25 h-full pb-12 md:col-span-10">
