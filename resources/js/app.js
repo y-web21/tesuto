d20b7aa535be82972db26143b9efb9c97920adf9
OdenMaster <y.web.2019@gmail.com>
2021-03-06 04:56:48 +0900

    編集画面のボタンにrequireとform methodの動的変更を適用

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,14 +20,18 @@ const patch = '<input type="hidden" name="_method" value="patch">';
 const post = '<input type="hidden" name="_method" value="post">';
 const required_field = ['new_title', 'new_content'];
 
-if (document.getElementById('btn_edit_confirm') != null) {
-    document.getElementById('btn_edit_confirm').addEventListener('click', { handleEvent: insertFormMethod, method: patch });
+if (document.getElementById('btn_submit_update') != null) {
+    document.getElementById('btn_submit_update').addEventListener('click', { handleEvent: insertFormMethod, method: patch });
 }
 if (document.getElementById('btn_edit_image_select') != null) {
     document.getElementById('btn_edit_image_select').addEventListener('click', { handleEvent: insertFormMethod, method: post });
 }
+
 if (document.getElementById('btn_submit_new_post') != null) {
     document.getElementById('btn_submit_new_post').addEventListener('click', { handleEvent: addRequired, target: required_field });
+}
+if (document.getElementById('btn_submit_update') != null) {
+    document.getElementById('btn_submit_update').addEventListener('click', { handleEvent: addRequired, target: required_field });
 }
 if (document.getElementById('btn_submit_select_image') != null) {
     document.getElementById('btn_submit_select_image').addEventListener('click', { handleEvent: removeRequired, target: required_field });
