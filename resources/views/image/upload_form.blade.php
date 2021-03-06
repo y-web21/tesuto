fb95720709370dae380c1dcf5df022baf500cdf5
OdenMaster <y.web.2019@gmail.com>
2021-03-06 20:14:37 +0900

    アップロードフォームのスタイル修正

--- resources/views/image/upload_form.blade.php
+++ resources/views/image/upload_form.blade.php
@@ -11,14 +11,23 @@ $disp_gnav = config('const.common.BLADE.GNAV.DISABLE');
     @include('poster.parts.layouts')
 
     <div class="bg-gray-400 sticky top-0 text-black text-center w-full mb-6 z-40">
-        <form class="p-5" action="{{ route('image.upload') }}" method="post" enctype="multipart/form-data">
+        <form class="p-5 lg:flex items-center" action="{{ route('image.upload') }}" method="post" enctype="multipart/form-data">
             @csrf
-            <input class="" type="file" name="image" accept="image/png, image/jpeg, image/png, image/gif">
-            {{-- <input type="file" name="image" accept="image/*"> --}}
-            @error('image')
-                {{ $message }}
-            @enderror
-            <button type="submit" class="btn-white">アップロード</button>
+            <div class="m-4 lg:w-full">
+            </div>
+            <div class="m-4 lg:w-full">
+                <input class="cursor-pointer" type="file" name="image" accept="image/png, image/jpeg, image/png, image/gif">
+                {{-- <input type="file" name="image" accept="image/*"> --}}
+            </div>
+            <div class="m-4  lg:w-full">
+                <button type="submit" class="btn-white">アップロード</button>
+            </div>
+            <div class="m-4 lg:w-full text-red-600">
+                @error('image')
+                    {{ $message }}
+                @enderror
+            </div>
+
         </form>
     </div>
 
@@ -31,7 +40,7 @@ $disp_gnav = config('const.common.BLADE.GNAV.DISABLE');
         </div>
 
         @error('page')
-            <p class="text-red-900">
+            <p class="text-red-600">
                 {{ $message }}
             </p>
         @enderror
@@ -48,15 +57,14 @@ $disp_gnav = config('const.common.BLADE.GNAV.DISABLE');
                 @foreach ($images as $image)
 
                     @if ($counter % 3 === 0)
-                        <div class="lg:flex mx-auto my-auto w-full justify-around align-bottom mt-8">
+                        <div class="lg:flex mx-auto my-auto w-full justify-around items-end mt-8">
                     @endif
 
-                    <div
-                        class="lg:m-4 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-lg bg-white my-12 mg:mx-8 w-full">
+                    <div class="lg:m-4 shadow-md hover:shadow-lg hover:bg-gray-100 rounded-lg bg-white my-12 w-full">
                         <!-- Card Image -->
                         <a href="{{ route('image.show', $image->id) }}">
-                        <img src="{{ asset('/storage/images/' . $image->name) }}" alt="{{ $image->description }}"
-                            class=" w-full object-contain"></a>
+                            <img src="{{ asset('/storage/images/' . $image->name) }}" alt="{{ $image->description }}"
+                                class=" w-full object-contain"></a>
 
                         <!-- Card Content -->
                         <div class="p-4 w-full">
@@ -66,32 +74,30 @@ $disp_gnav = config('const.common.BLADE.GNAV.DISABLE');
                                 <form action={{ route('image.del-req') }} method="post" style="display:inline-block;">
                                     @csrf
                                     <input type="hidden" name="id" value="{{ $image->id }}">
-                                    <button type="submit"
-                                        class="btn-red">削除</button>
+                                    <button type="submit" class="btn-red">削除</button>
                                 </form>
                             </div>
                         </div>
                     </div>
 
                     @if ($counter % 3 === 2)
-                        </div>
-                    @endif
-
-                    @php
-                        $counter++;
-                    @endphp
-                @endforeach
-
-                @if ($counter % 3 !== 2)
-                    @for ($i = 0; $i <= $counter % 3; $i++)
-                        <div
-                            class="lg:m-4 my-12 mg:mx-8 w-full">
-                        </div>
-                    @endfor
-                    </div>
-                @endif
             </div>
+            @endif
+
+            @php
+                $counter++;
+            @endphp
+            @endforeach
+
+            @if ($counter % 3 !== 2)
+                @for ($i = 0; $i <= $counter % 3; $i++)
+                    <div class="lg:m-4 my-12 w-full">
+                    </div>
+                @endfor
         </div>
+        @endif
+    </div>
+    </div>
     </div>
 
     {{ $images->links('layouts.paginator.default') }}
