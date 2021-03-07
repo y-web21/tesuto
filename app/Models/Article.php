aaf9c550352e2acc31d97209d90f8a6c4b4fca94
y-web21 <y.web.2019@gmail.com>
2021-03-08 00:08:40 +0900

    テストコード削除

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -11,15 +11,6 @@ class Article extends Model
     use HasFactory;
     protected $fillable = ['title', 'content', 'author'];
 
-    public function statusName(){
-        return $this->hasOne(ArticleStatus::class);
-        return $this->hasOne(ArticleStatus::class, 'id');
-    }
-
-    public function test(){
-        return $this->id;
-    }
-
     /**
      * Get the uploadImage associated with the Article
      *
