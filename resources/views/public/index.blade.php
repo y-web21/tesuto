9f3f762a9b85a1fae5c9c75e32f297c8674a7fca
OdenMaster <y.web.2019@gmail.com>
2021-03-08 00:15:37 +0900

    ホームレイアウト作成

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,18 +1,66 @@
 @extends('layouts.master')
 @section('title', 'home')
 
-@php
-$disp_header = config('const.common.BLADE.HEADER.LARGE');
-@endphp
+    @php
+    $disp_header = config('const.common.BLADE.HEADER.LARGE');
+    @endphp
 
 @section('content')
-    @foreach ($articles as $article)
-        {{ $article->title }}
-        <br>
-        {{ Helper::strlimit($article->content, 50) }}
-    @endforeach
+
+<div class="px-4 py-6">
+
+    <section class="lg:grid gap-4 lg:grid-cols-3 w-full my-10">
+        <div style="grid-column: 1 / 3;">
+            <div class="bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg w-full p-5 h-100per">
+                @if ($top_news !== null)
+                <a class="flex justify-between" href={{ route('article.show', ['article' => $top_news->id]) }}>
+                    <div class="flex flex-col mx-5">
+                        <h3 class="text-xl font-semibold mb-2">{{ $top_news->title }}</h3>
+                        <p class="text-gray-800 mt-2">{{ Helper::strlimit($top_news->content, 150) }}</p>
+                        <p class="ml-3 mt-auto">{{ $top_news->created_at }}</p>
+                    </div>
+                    @if ($top_news->uploadImage === null)
+                        <img alt="no_image" class="rounded-md border-2 border-gray-300 object-cover w-200px h-200px"
+                            src=" {{ asset('storage/images/no_image.png') }}">
+                    @else
+                        <img alt="{{ $top_news->uploadImage->description }}"
+                            class="rounded-md border-2 border-gray-300 object-cover w-200px h-200px"
+                            src="{{ asset('storage/images/' . $top_news->uploadImage->name) }}">
+                    @endif
+                </a>
+                @else
+                <p>no posts.</p>
+                @endif
+            </div>
+        </div>
+        <div style="grid-column: 3 / 4;">
+            <div class="bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg p-5 my-10 lg:my-0 h-100per">
+                {{ 'void' }}
+            </div>
+        </div>
+    </section>
+
+    <section class="grid gap-7 sm:grid-cols-1 lg:grid-cols-2 w-full my-10">
+        @foreach ($articles as $article)
+            <div class="bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg w-full p-5">
+                <a class="flex justify-between" href={{ route('article.show', ['article' => $article->id]) }}>
+                    <div class="flex flex-col mx-5">
+                        <h3 class="text-xl font-semibold mb-2">{{ $article->title }}</h3>
+                        <p class="text-gray-800 mt-2">{{ Helper::strlimit($article->content, 50) }}</p>
+                        <p class="ml-3 mt-auto">{{ $article->created_at }}</p>
+                    </div>
+                    @if ($article->uploadImage === null)
+                        <img alt="no_image" class="rounded-md border-2 border-gray-300 object-cover w-150px h-150px"
+                            src=" {{ asset('storage/images/no_image.png') }}">
+                    @else
+                        <img alt="{{ $article->uploadImage->description }}"
+                            class="rounded-md border-2 border-gray-300 object-cover w-1/3 h-150px"
+                            src="{{ asset('storage/images/' . $article->uploadImage->name) }}">
+                    @endif
+                </a>
+            </div>
+        @endforeach
+    </section>
+</div>
 @endsection
 
-@section('footer')
-    @parent
-@endsection
