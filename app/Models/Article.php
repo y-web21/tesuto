88cace408f8e6cbe9532e22e368ec6ec9adf873c
OdenMaster <y.web.2019@gmail.com>
2021-03-07 23:43:39 +0900

    リレーションの設定

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Article extends Model
 {
@@ -17,5 +18,35 @@ class Article extends Model
 
     public function test(){
         return $this->id;
+    }
+
+    /**
+     * Get the uploadImage associated with the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function uploadImage(): HasOne
+    {
+        return $this->hasOne(UploadImage::class,'id' ,'featured_image_id');
+    }
+
+    /**
+     * Get the user associated with the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'author');
+    }
+
+    /**
+     * Get the articleStatus associated with the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function articleStatus(): HasOne
+    {
+        return $this->hasOne(ArticleStatus::class, 'status_id', 'status');
     }
 }
