8b6c129e3fd2bc071561a0415a579930caa79a19
OdenMaster <y.web.2019@gmail.com>
2021-03-08 03:59:39 +0900

    element styleをcssに変更

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('title', 'home')
+@section('title', 'Fictitious companany')
 
     @php
     $disp_header = config('const.common.BLADE.HEADER.LARGE');
@@ -7,12 +7,9 @@
 
 @section('content')
 
-<div class="px-4 py-6">
-
-    <section class="lg:grid gap-4 lg:grid-cols-3 w-full my-10">
-        <div style="grid-column: 1 / 3;">
-            <div class="bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg w-full p-5 h-100per">
-                @if ($top_news !== null)
+    <section class="lg:grid lg:grid-cols-3 gap-4 w-full">
+        <div class="lg:col-span-2 bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg w-full p-5 h-100per">
+            @if ($top_news !== null)
                 <a class="flex justify-between" href={{ route('article.show', ['article' => $top_news->id]) }}>
                     <div class="flex flex-col mx-5">
                         <h3 class="text-xl font-semibold mb-2">{{ $top_news->title }}</h3>
@@ -28,19 +25,17 @@
                             src="{{ asset('storage/images/' . $top_news->uploadImage->name) }}">
                     @endif
                 </a>
-                @else
+            @else
                 <p>no posts.</p>
-                @endif
-            </div>
+            @endif
         </div>
-        <div style="grid-column: 3 / 4;">
-            <div class="bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg p-5 my-10 lg:my-0 h-100per">
-                {{ 'void' }}
-            </div>
+        <div
+            class="lg:col-span-1 bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg p-5 my-10 lg:my-0 h-100per">
+            {{ 'void' }}
         </div>
     </section>
 
-    <section class="grid gap-7 sm:grid-cols-1 lg:grid-cols-2 w-full my-10">
+    <section class="grid sm:grid-cols-1 gap-7 lg:grid-cols-2 w-full my-10">
         @foreach ($articles as $article)
             <div class="bg-white border-2 border-gray-300 rounded-md tracking-wide shadow-lg w-full p-5">
                 <a class="flex justify-between" href={{ route('article.show', ['article' => $article->id]) }}>
@@ -61,6 +56,5 @@
             </div>
         @endforeach
     </section>
-</div>
-@endsection
 
+@endsection
